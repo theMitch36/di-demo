0968cfb28d67d8f7c725ed8f50c8d68e33975d53
theMitch36 <devmakos36@gmail.com>
2018-02-22 12:48:59 -0500

    spring profiles

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -17,8 +17,8 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		controller.hello();
 
+		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
---
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -0,0 +1,17 @@
+package guru.springframework.services;
+
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("es")
+@Primary
+public class PrimarySpanishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Servicio de Saludo Primrio";
+    }
+}
